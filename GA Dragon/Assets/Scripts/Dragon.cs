ba54a58c4e959cb3e627a8b4c1130e873b34eda8
Piorosen <aoikazto@gmail.com>
2018-12-23 01:17:14 +0900

    새로운 하반신 적용중

--- GA Dragon/Assets/Scripts/Dragon.cs
+++ GA Dragon/Assets/Scripts/Dragon.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using GeneticType = System.Collections.Generic.List<System.Collections.Generic.List<float>>;
 public class Dragon : MonoBehaviour
 {
     List<Arm> Arms;
     public int State = 0;
-    public List<float> Genetic1;
-    public List<float> Genetic2;
+    public GeneticType Genetic;
 
-    public float Power;
+    int GeneticSize = 4;
+    List<float> Move = new List<float>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,45 +25,90 @@ public class Dragon : MonoBehaviour
             };
             Arms.Add(arm);
         }
-        
-        CreateGenetic();
-        StartCoroutine(Runnig());
+        StartCoroutine(Running());
     }
     
 
-    void CreateGenetic()
+    public void CreateGenetic(float transition = 0, GeneticType geneticData = null)
     {
-        Genetic1.Clear();
-        Genetic2.Clear();
+        Genetic = new List<List<float>>();
+        Genetic.Clear();
+        if (geneticData == null)
+        {
+            for (int k = 0; k < GeneticSize; k++)
+            {
+                List<float> tmp = new List<float>();
+                for (int i = 0; i < 4; i++)
+                {
+                    tmp.Add(Random.Range(-20.0f, 40.0f));
+                    tmp.Add(Random.Range(-80.0f, 80.0f));
+                }
+                Genetic.Add(tmp);
+            }
+        }
+        else
+        {
+            Genetic = geneticData;
 
-        for (int i = 0; i < 4; i++)
-        {
-            Genetic1.Add(Random.Range(-30.0f, 30.0f));
-            Genetic1.Add(Random.Range(-60.0f, 60.0f));
-        }
-        for (int i = 0; i < 4; i++)
-        {
-            Genetic2.Add(Random.Range(-30.0f, 30.0f));
-            Genetic2.Add(Random.Range(-60.0f, 60.0f));
+            for (int i = 0; i < Genetic.Count; i++)
+            {
+                for (int k = 0; k < Genetic[i].Count; k++)
+                {
+                    Genetic[i][k] += Random.Range(-10.0f, 10.0f) * transition;
+                }
+            }
+            
         }
     }
-    IEnumerator Runnig()
+
+    IEnumerator Running()
     {
-        while (true)
+        for (int i = 0; i < 4; i++)
+        {
+            var t = new JointMotor
+            {
+                force = 10000,
+                freeSpin = false,
+                targetVelocity = Genetic[State][i * 2] > Arms[i].Leg.transform.rotation.x ? 1000 : -1000
+            };
+            // Arms[i].Leg.GetComponent<HingeJoint>().motor = t;
+            Arms[i].Leg.transform.RotateAround(Vector3.zero, new Vector3(1, 0, 0), Genetic[State][i * 2]);
+            t.targetVelocity = Genetic[State][i * 2 + 1];
+            // Arms[i].LegHinge.GetComponent<HingeJoint>().motor = t;
+            Arms[i].Leg.transform.RotateAround(Vector3.zero, new Vector3(1, 0, 0), Genetic[State][i * 2 + 1]);
+        }
+    }
+
+    int frame = 0;
+    private void FixedUpdate()
+    {
+        frame++;
+        if (Move.Count != 0)
         {
             for (int i = 0; i < 4; i++)
             {
-                Arms[i].Leg.transform.Rotate(new Vector3(Genetic1[i * 2], 0,0));
-                Arms[i].LegHinge.transform.Rotate(new Vector3(Genetic1[i * 2 + 1], 0, 0));
+                Arms[i].Leg.transform.Rotate(new Vector3(1, 0, 0), Move[i * 2] * Time.fixedDeltaTime * 5);
+                Arms[i].Leg.transform.Rotate(new Vector3(1, 0, 0), Move[i * 2 + 1] * Time.fixedDeltaTime * 5);
             }
-            yield return new WaitForSeconds(0.2f);
+        }
+        if (frame % 10 == 0)
+        {
+            State++;
+            Move.Clear();
+            if (State == Genetic.Count)
+            {
+                State = 0;
+            }
+
+
             for (int i = 0; i < 4; i++)
             {
-                Arms[i].Leg.transform.Rotate(new Vector3(Genetic2[i * 2], 0, 0));
-                Arms[i].LegHinge.transform.Rotate(new Vector3(Genetic2[i * 2 + 1], 0, 0));
+                Move.Add(Arms[i / 2].Leg.transform.rotation.x - Genetic[State][i]);
+                Move.Add(Arms[i / 2].LegHinge.transform.rotation.x - Genetic[State][i + 1]);
             }
-            yield return new WaitForSeconds(0.2f);
+            frame = 0;
         }
-    }
+
+    }    
 
 }
--- GA Dragon/Assets/Scripts/GAManager.cs
+++ GA Dragon/Assets/Scripts/GAManager.cs
@@ -2,17 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using GeneticType = System.Collections.Generic.List<System.Collections.Generic.List<float>>;
 public class GAManager : MonoBehaviour
 {
+    public GameObject Dragon;
+
+    List<Dragon> GenerateDragon;
+    List<GeneticType> LivedDragon;
+
+    public int CreateDragon = 20;
+    public int Gen = 0;
+    public float LiveRatio = 0.25f;
+    public float Transition = 0.2f;
+    public float NextGenTime = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
+        GenerateDragon = new List<Dragon>();
+        LivedDragon = new List<GeneticType>();
+        StartCoroutine(Run());
+    }
+
+    IEnumerator Run()
+    {
+        while (true)
+        {
+            NextGenerate();
+            yield return new WaitForSeconds(NextGenTime);
+        }
+    }
+
+    void NextGenerate()
+    {
+        Gen++;
+        if (GenerateDragon.Count != 0)
+        {
+            GenerateDragon.Sort((x, y) =>
+            {
+                var Com1 = x.transform.GetChild(0).transform.position;
+                var Com2 = y.transform.GetChild(0).transform.position;
+                if (Com1.z > Com2.z)
+                {
+                    return -1;
+                }
+                else if (Com1.z < Com2.z)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            });
+            Debug.Log($"이전 세대 최고 치 : {Gen - 1}\n{GenerateDragon[0].transform.GetChild(0).transform.position.z}");
+            SurviveDragon();
+        }
+        CrossDragon();
+    }
+
+    void SurviveDragon()
+    {
+        for (int i = 0; i < (int)(GenerateDragon.Count * LiveRatio); i++)
+        {
+            LivedDragon.Add(GenerateDragon[i].Genetic);
+        }
+        for (int i = 0; i < GenerateDragon.Count; i++)
+        {
+            Destroy(GenerateDragon[i].gameObject);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void CrossDragon()
     {
-        
+        Debug.Log(Transition);
+        GenerateDragon.Clear();
+        for (int k = 0; k < CreateDragon; k++)
+        {
+            float Position = k * 10;
+            var data = Instantiate(
+                        Dragon,
+                        new Vector3(Position, 1.3f, 0.0f),
+                        Quaternion.identity).GetComponent<Dragon>();
+            if (LivedDragon.Count == 0)
+            {
+                data.CreateGenetic();
+            }
+            else
+            {
+                data.CreateGenetic(Transition, LivedDragon[(int)(k / (1.0f / LiveRatio))]);
+            }
+
+            GenerateDragon.Add(data);
+        }
+        LivedDragon.Clear();
+        Debug.Log("생성끝");
     }
+        
 }
