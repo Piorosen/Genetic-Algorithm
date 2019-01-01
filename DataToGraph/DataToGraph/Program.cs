416995fa4eedc7683423e14da6a2efa07c5d12f1
Piorosen <aoikazto@gmail.com>
2019-01-02 02:02:41 +0900

    분석 프로그램 추가

---
+++ DataToGraph/DataToGraph/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DataToGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("result.csv", false, Encoding.UTF8);
+            sw.WriteLine("Gen, Move");
+            for (int i = 0; i < 215; i++)
+            {
+                using (StreamReader sr = new StreamReader($@"data\{i + 1}.txt"))
+                {
+                    var data = sr.ReadLine().Split(' ')[3];
+                    sw.WriteLine($"{i + 1}, {data}");
+                    sr.Close();
+                }
+            }
+            sw.Close();
+        }
+    }
+}
